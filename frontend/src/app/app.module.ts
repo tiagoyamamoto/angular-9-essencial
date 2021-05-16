765852876e04a42abc4ad3c20f8c0a55e7f4982f
Tiago Yamamoto <japa@rede5.com.br>
2021-05-15 23:02:11 -0300

    Diretivas na Prática #01

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
+    RedDirective,
   ],
   imports: [
     BrowserModule,
