d440fc27bfe71c595a2dab646b603d8c1c9a9406
Tiago Yamamoto <japa@rede5.com.br>
2021-05-17 23:30:01 -0300

    Componente Excluir Produto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 const routes: Routes = [
   {
@@ -23,6 +23,10 @@ const routes: Routes = [
   {
   path: "products/update/:id",
   component: ProductUpdateComponent,
+  },
+  {
+  path: "products/delete/:id",
+  component: ProductDeleteComponent,
   }
 ];
 
